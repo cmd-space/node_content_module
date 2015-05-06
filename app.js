0f54066aad290d12961b57746d3079fa9162c9ec
mec64 <masonecrane@gmail.com>
2015-05-06 13:34:12 -0600

    got module working, thanks to Andrew

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 // http server
 var http = require('http');
-var fs = require('fs');
+//var fs = require('fs');
 
-var static_contents = require('./static.js')();
+var static_contents = require('./static.js');
 
 //creating a server
 server = http.createServer(function (request, response){
+//    response.writeHead(200, {'Content-type': 'text/html'});
+//    console.log('Request', request.url);
+//    if(request.url === '/'){
+//    fs.readFile('views/index.html', 'utf8', function (errors, contents){
+//      response.write(contents); 
+//      response.end();
+//    });
     static_contents(request, response);  //this will serve all static files automatically
-});
+})
 server.listen(8000);
 console.log("Running in localhost at port 8000");
--- static.js
+++ static.js
@@ -1,5 +1,17 @@
-module.exports = function(){
-    return{
-        
+var fs = require('fs');
+
+module.exports = function ( request, response ) {
+    response.writeHead(200, {'Content-type': 'text/html'});
+    console.log('Request', request.url);
+    if(request.url === '/'){
+        fs.readFile('views/index.html', 'utf8', function (errors, contents){
+            response.write(contents); 
+            response.end();
+        });
+    } else if (request.url === '/ninja') {
+        fs.readFile('views/ninja.html', 'utf8', function (errors, contents){
+            response.write(contents);
+            response.end('Hi There, Ninja');
+        })
     }
 }
